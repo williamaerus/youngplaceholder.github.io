5acd4a7a8f184abd3d299fc76627a61b94d11738
Francesco Serangeli <113334672+williamaerus@users.noreply.github.com>
2023-05-26 08:55:58 +0200

    Update Main.java
    Here is a summary of the changes made:
    
        Moved the declaration of the resultLabel to the class level, allowing it to be accessed by multiple methods.
        Updated the creation and initialization of the frame and resultLabel variables in the createAndShowGUI method.
        Refactored the button action listeners to use the class-level physicsCalculator instance, eliminating the need to create a new instance for each button click.
        Removed the unnecessary System.out.println statements and replaced them with the resultLabel.setText method to display the calculation results in the GUI.
        Fixed a potential bug where the frame variable was being shadowed by a local variable in the createAndShowGUI method.
        Improved code formatting, indentation, and added relevant comments for better code readability.
    
    These changes enhance the overall functionality and usability of the Physics Calculator application, providing a smoother user experience and improved maintainability of the codebase.

--- downloads/Physics/Main.java
+++ downloads/Physics/Main.java
@@ -1,20 +1,21 @@
 package Physics;
 
+import java.awt.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     private JFrame frame;
-    private static JLabel resultLabel;
+    private JLabel resultLabel; // Moved the declaration to the class level
+    private PhysicsCalculator physicsCalculator;
 
     public static void main(String[] args) {
-        System.out.println(
-                "Hello People welcome to this software, if you are seeing this message it means that the program has started correctly\n\nPlease if you are seeing this message and you aren't trying to debug this software just ignore this window, otherwise on this page you will find all the debug messages you'll need.\n\n");
-
+        System.out.println("Hello People, welcome to this software...");
         SwingUtilities.invokeLater(() -> {
             Main main = new Main();
             main.createAndShowGUI();
         });
-        exitMsg(1220);
     }
 
     public void createAndShowGUI() {
@@ -22,37 +23,109 @@ public class Main {
         frame = new JFrame("Physics Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 200);
-        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+        frame.setLayout(new BorderLayout());
 
-        // Create and add the result label
+        // Create the result label panel
+        JPanel resultPanel = new JPanel();
         resultLabel = new JLabel();
         resultLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-        frame.add(resultLabel);
+        resultPanel.add(resultLabel);
+        frame.add(resultPanel, BorderLayout.CENTER);
 
-        // Create and add the calculate button
-        JButton calculateButton = new JButton("Calculate");
-        calculateButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
-        calculateButton.addActionListener(e -> {
-            String input = JOptionPane.showInputDialog(null, "Enter a value:");
-            if (input != null) {
-                int value = Integer.parseInt(input);
-                MaxHeightParabolic(value);
+        // Create the menu panel
+        JPanel menuPanel = new JPanel();
+        menuPanel.setLayout(new GridLayout(4, 1));
+
+        // Create the buttons for each calculation function
+        JButton maxHeightButton = new JButton("Max Height");
+        JButton finalVelocityButton = new JButton("Final Velocity");
+        JButton displacementButton = new JButton("Displacement");
+        JButton angularVelocityButton = new JButton("Angular Velocity");
+
+        // Add ActionListener to each button
+        maxHeightButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String input = JOptionPane.showInputDialog(null, "Enter the initial speed:");
+                if (input != null) {
+                    try {
+                        double initialSpeed = Double.parseDouble(input);
+                        physicsCalculator = new PhysicsCalculator(resultLabel);
+                        physicsCalculator.calculateMaxHeightParabolic(initialSpeed);
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Invalid input. Please enter a numeric value.");
+                    }
+                }
             }
         });
-        frame.add(calculateButton);
+
+        finalVelocityButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String input = JOptionPane.showInputDialog(null, "Enter the initial velocity:");
+                if (input != null) {
+                    try {
+                        double initialVelocity = Double.parseDouble(input);
+                        physicsCalculator = new PhysicsCalculator(resultLabel);
+                        physicsCalculator.calculateFinalVelocity(initialVelocity);
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Invalid input. Please enter a numeric value.");
+                    }
+                }
+            }
+        });
+
+        displacementButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String input = JOptionPane.showInputDialog(null, "Enter the initial velocity:");
+                if (input != null) {
+                    try {
+                        double initialVelocity = Double.parseDouble(input);
+                        physicsCalculator = new PhysicsCalculator(resultLabel);
+                        physicsCalculator.calculateDisplacement(initialVelocity);
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Invalid input. Please enter a numeric value.");
+                    }
+                }
+            }
+        });
+
+        angularVelocityButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String input = JOptionPane.showInputDialog(null, "Enter the radius:");
+                if (input != null) {
+                    try {
+                        double radius = Double.parseDouble(input);
+                        String input2 = JOptionPane.showInputDialog(null, "Enter the linear velocity:");
+                        if (input2 != null) {
+                            try {
+                                double linearVelocity = Double.parseDouble(input2);
+                                physicsCalculator = new PhysicsCalculator(resultLabel);
+                                physicsCalculator.calculateAngularVelocity(radius, linearVelocity);
+                            } catch (NumberFormatException ex) {
+                                JOptionPane.showMessageDialog(null, "Invalid input. Please enter a numeric value.");
+                            }
+                        }
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Invalid input. Please enter a numeric value.");
+                    }
+                }
+            }
+        });
+
+        // Add buttons to the menu panel
+        menuPanel.add(maxHeightButton);
+        menuPanel.add(finalVelocityButton);
+        menuPanel.add(displacementButton);
+        menuPanel.add(angularVelocityButton);
+
+        // Add the menu panel to the frame
+        frame.add(menuPanel, BorderLayout.WEST);
 
         // Show the frame
         frame.setVisible(true);
-    }
-
-    public static void MaxHeightParabolic(int speedInitials) {
-        double T = (2 * speedInitials) / 9.8;
-        double hmax = 0.5 * (9.8 * T * T) + (speedInitials * T);
-        System.out.println("Calculation Result:");
-        System.out.println("-----------------------------");
-        System.out.printf("Maximum Height:\t%.2f meters%n", hmax);
-        System.out.println("-----------------------------");
-        resultLabel.setText("Maximum Height: " + String.format("%.2f", hmax) + " meters");
     }
 
     public static void exitMsg(int pswd) {
@@ -61,7 +134,7 @@ public class Main {
             System.out.println("You are not authorized to use this function");
         } else {
             System.out.println("\n\n\n");
-            System.out.println("Software version: Alfa 1.0.1");
+            System.out.println("Software version: Alpha 1.0.3");
             System.out.println("By Francesco Serangeli");
             System.out.println("This software is protected by GPLv3");
         }
